961c5bc905ec1a008c465b0f35c28bc254839fa6
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-24 02:34:41 +0300

    scrape3

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, scrape)
+                    send_message(sender_id, scrape())
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
