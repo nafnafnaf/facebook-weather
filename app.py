f3e7dad65400712aab25f4ea02c46dc55f697511
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-23 14:41:21 +0300

    string

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def webhook():
                     pass
 
     return "ok", 200
-x = str[1, 3, 3, 90]
+x = "aladslkj"
 
 def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
