0b06fc91709b60b9fedd8c82ad6471549012f246
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-25 14:52:59 +0300

    attach

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def webhook():
                     if "attachment" in messaging_event["message"]:
                         message_text = message_text["message"]["attachment"]
 
-                    send_message(sender_id, sc)
+                    send_message(sender_id, message_text)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
