b1e41efc783bc4256e6cdbe401de47b9117c8243
dimi nafpigos <dimi@dimi-ThinkPad-X61s.lan>
2017-10-25 14:29:52 +0300

    attach

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def send_message(recipient_id, message_text):
              "url":"http://www.messenger-rocks.com/image.jpg" 
               
                   }
-               }
+               }}
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
